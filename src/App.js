29cd27bc5afddb94e8a34ef6a091bfe8bc760c42
Pedro Lopes <pedroppzs01@gmail.com>
2025-01-24 16:34:23 -0300

    Adiciona estilização responsiva e animação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {useState} from 'react'
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
 function App() {
+  const [inuput, setInput] = useState('');
+
+function handleSearch(){
+  alert("VALOR DO INPUT  " + inuput)
+}
+
   return (
     <div className="conteiner">
       <h1 className="title">Buscador CEP</h1>
@@ -8,20 +15,20 @@ function App() {
         <input
           type="text"
           placeholder="Digite o CEP..."
+          value={inuput}
+          onChange={(e) => setInput(e.target.value)}
         />
-        <button className="buttonSearch">
-          <FiSearch size={25} color='#FFF'/>
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#FFF' />
         </button>
-        <main className='main'>
-
-          <h2>CEP: 0972359482</h2>
-          <span>Rua teste algum</span>
-          <span>Complemento: Algum Complemento</span>
-          <span>Vila Rosa</span>
-          <span>Campo Grande - MS</span>
-
- </main>
       </div>
+      <main className='main'>
+        <h2>CEP: 0972359482</h2>
+        <span>Rua teste algum</span>
+        <span>Complemento: Algum Complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
     </div>
   );
 }
